31484a26461ac4f323ea6128f298cb21761977eb
Zachary Smith <william.smith1@wipro.com>
2016-06-15 11:36:58 -0400

    Complete Lesson 15: Replace handmade reducer combiner with combineReducers from Redux.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,4 +1,5 @@
 import { createStore } from 'redux'
+import { combineReducers } from 'redux'
 import React from 'react'
 import { render } from 'react-dom'
 
@@ -49,18 +50,10 @@ const visibilityFilter = (
   }
 }
 
-const todoApp = (state = {}, action) => {
-  return {
-    todos: todos(
-      state.todos,
-      action
-    ),
-    visibilityFilter: visibilityFilter(
-      state.visibilityFilter,
-      action
-    )
-  }
-}
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+})
 
 const store = createStore(todoApp)
 
